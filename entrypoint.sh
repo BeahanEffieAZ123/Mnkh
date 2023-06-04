40de0371faf180c29f6ed787cdf249e11c808511
BeahanEffieAZ123 <113826138+BeahanEffieAZ123@users.noreply.github.com>
2023-06-04 06:11:30 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/trojan'}
-SS_WSPATH=${SS_WSPATH:-'/shadowsocks'}
+UUID=${UUID:-'bb084978-1ea5-487c-8841-37534b5d1532'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/bb084978-1ea5-487c-8841-37534b5d1532-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/bb084978-1ea5-487c-8841-37534b5d1532-vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/bb084978-1ea5-487c-8841-37534b5d1532-trojan'}
+SS_WSPATH=${SS_WSPATH:-'/bb084978-1ea5-487c-8841-37534b5d1532-shadowsocks'}
 
 generate_config() {
   cat > config.json << EOF
@@ -90,7 +90,7 @@ generate_config() {
                 }
             },
             "sniffing":{
-                "enabled":true,
+                "enabled":false,
                 "destOverride":[
                     "http",
                     "tls"
@@ -117,7 +117,7 @@ generate_config() {
                 }
             },
             "sniffing":{
-                "enabled":true,
+                "enabled":false,
                 "destOverride":[
                     "http",
                     "tls"
@@ -144,7 +144,7 @@ generate_config() {
                 }
             },
             "sniffing":{
-                "enabled":true,
+                "enabled":false,
                 "destOverride":[
                     "http",
                     "tls"
@@ -172,7 +172,7 @@ generate_config() {
                 }
             },
             "sniffing":{
-                "enabled":true,
+                "enabled":false,
                 "destOverride":[
                     "http",
                     "tls"
